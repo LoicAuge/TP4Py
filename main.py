9ce482c4be3073451f0b8e6fbb721e2e8b5a60d5
Loïc Augé <loic.auge@limayrac.fr>
2020-06-22 16:06:46 +0000

    TP4

--- main.py
+++ main.py
@@ -1,54 +1,49 @@
-"""annee =	['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre',10,11,12]
-annee.remove(10)
-annee.remove(11)
-annee.remove(12)
-print(annee)
-annee.append("Octobre")
-annee.append("Novembre")
-annee.append("Decembre")
-print(annee)"""
+"""x = 1
+while x < 50 :
+  print('Facile ! '+ str(x))
+  x+=1"""
 
-"""annee =	['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre',10,11,12]
-annee[9] = "Octobre"
-annee[10] = "Novembre"
-annee[11] = "Decembre"
-print(annee)"""
+"""x=1
+while x<25:
+  print('*',end='')
+  x+=1"""
 
-"""moisDeLannee = ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
-print(moisDeLannee[3])
-print("mars" in moisDeLannee)"""
+"""for x in range(21,146):
+  print(x)"""
 
-"""age = {"pierre" : 35 , "paul" : 32 , "Jacques" : 27 , "andre" : 23}
-age["David"] = 22
-age["Veronique"] = 21
-age["Sylvie"] = 30
-age["Damien"] = 37
-print(age)
-print(age["Sylvie"])
-if 'jean' in age:
-  print("Jean présent")
+"""for x in range(1,41):
+  print('le carre de '+str(x)+' est '+str(x**2))"""
+
+"""y=0
+for x in range(21,146):
+  y+= x
+  print(y)"""
+
+"""nbr = 35
+fact = 1
+for i in range(1, nbr+1):
+  fact = fact * i
+print (nbr,'! = ',fact)"""
+
+"""nb =1
+for x in range(15):
+  for y in range(nb):
+    print('*',end='')
+  print('')
+  nb+=1"""
+
+dico = {}
+rep = 'o'
+while rep == 'o':
+    print("Saisir un mot anglais.")
+    ang = input('> ')
+    print("Saisir la traduction française.")
+    fr = input('> ')
+    dico[ang] = fr
+    print("Voulez-vous saisir une nouvelle entrée ?")
+    rep = input('> ')
 else:
-  print("Jean absent")"""
+    for k, v in dico.items():
+        print(k, v)
 
-"""club = {}
-club['pierre durand']=(1986,1.72,70)
-club['victor dupont']=(1987,1.89,57)
-club['paul dupuis']=(1989,1.60,92)
-club['jean rieux']=(1985,1.88,77)
-print(club)
-dateNaissSportif = club['paul dupuis'][0]
-TailleSportif = club['paul dupuis'][1]
-poidsSportif = club['paul dupuis'][2]
-formatDonnees = 'Le sportif nommé Paul Dupuis est né en %s, sa taille est de %s m et son poids est de %s kg.'
-print(formatDonnees %(dateNaissSportif,TailleSportif,poidsSportif))"""
-
-club = {'pierre durand': (1986, 1.72, 70), 'victor dupont': (1987, 1.89, 57), 'paul dupuis': (1989, 1.6, 92), 'jean rieux': (1985, 1.88, 77)}
-formatDonnees = 'Le sportif nommé %s est né en %s, sa taille est de %s m et son poids est de %s kg.'
-name = input("> ")
-if name in club:
-  dateNaissSportif = club[name][0]
-  TailleSportif = club[name][1]
-  poidsSportif = club[name][2]
-  print(formatDonnees %(name,dateNaissSportif,TailleSportif,poidsSportif))
-else:
-  print(name+" inexistant")
+        
